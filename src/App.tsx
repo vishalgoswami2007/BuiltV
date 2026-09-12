d938f8385670e9a3bfe4c776ff61550e6a099075
Vishal Goswami <vg0767875@gmail.com>
2026-09-12 11:24:24 +0530

    chore: initialize BuiltV frontend

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-[#050608] text-[#F8FAFC]">
+      <h1 className="p-8 text-3xl font-semibold">BuiltV</h1>
+    </main>
+  );
+}
+
+export default App;
