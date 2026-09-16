0201edf4ced0dd077fe087295f58540efff269d1
Vishal Goswami <vg0767875@gmail.com>
2026-09-16 17:52:35 +0530

    feat: build BuiltV personalized demo configurator

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import Services from "../pages/Services/Services";
 import Work from "../pages/Work/Work";
 import Solutions from "../pages/Solutions/Solutions";
 import Pricing from "../pages/Pricing/Pricing";
+import Demo from "../pages/Demo/Demo";
 import About from "../pages/About/About";
 import Insights from "../pages/Insights/Insights";
 import Contact from "../pages/Contact/Contact";
@@ -23,6 +24,7 @@ function AppRoutes() {
       <Route path="/work" element={<Work />} />
       <Route path="/solutions" element={<Solutions />} />
       <Route path="/pricing" element={<Pricing />} />
+      <Route path="/demo" element={<Demo />} />
       <Route path="/about" element={<About />} />
       <Route path="/insights" element={<Insights />} />
       <Route path="/contact" element={<Contact />} />
